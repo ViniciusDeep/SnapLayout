32504fdfb62ca7e0b421980ae4c2a235c3180dc0
Vinicius Mangueira <viniciuscorreia11@gmail.com>
2019-09-08 18:19:57 -0300

    Start project SnapLayout

---
+++ SnapLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapLayout
+//
+//  Created by Vinicius Mangueira on 08/09/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
