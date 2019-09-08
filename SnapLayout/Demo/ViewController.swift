c5ef674341d035dccd3a7df4cf5abd1c2af8b0cc
Vinicius Mangueira <viniciuscorreia11@gmail.com>
2019-09-08 18:40:25 -0300

    Start implement SnapLayout

---
+++ SnapLayout/Demo/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  SnapLayout
+//
+//  Created by Vinicius Mangueira on 08/09/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: SnapLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollection()
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    fileprivate func setupCollection() {
+        self.collectionView.backgroundColor = #colorLiteral(red: 0.4941176471, green: 0.1411764706, blue: 0.6117647059, alpha: 1)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .white
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: self.view.frame.width - 48, height: self.view.frame.height/2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 100, left: 30, bottom: 0, right: 0)
+    }
+    
+    
+}
+
---
+++ SnapLayout/Source/SnapLayout.swift
@@ -0,0 +1,89 @@
+//
+//  SnapLayout.swift
+//  SnapLayout
+//
+//  Created by Vinicius Mangueira on 08/09/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+class SnapLayout: UICollectionViewController {
+    
+    init() {
+        let layout = BetterSnappingLayout()
+        layout.scrollDirection = .horizontal
+        super.init(collectionViewLayout: layout)
+        collectionView.decelerationRate = .fast
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class SnappingLayout: UICollectionViewFlowLayout {
+    // Efeito snap
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        guard let collectionView = collectionView else { return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity) }
+        let parent = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+        
+        // Número do scroll
+        let itemWidth = collectionView.frame.width - 100
+        
+        let itemSpace = itemWidth + minimumInteritemSpacing
+        var pageNumber = round(collectionView.contentOffset.x / itemSpace)
+        
+        let vX = velocity.x
+        if vX > 0 {
+            pageNumber += 1
+        } else if vX < 0 {
+            pageNumber -= 1
+        }
+        
+        let nearestPageOffset = pageNumber * itemSpace
+        return CGPoint(x: nearestPageOffset,
+                       y: parent.y)
+    }
+    
+}
+
+
+class BetterSnappingLayout: UICollectionViewFlowLayout {
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        
+        guard let collectionView = collectionView else {
+            return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+        }
+        
+        let nextX: CGFloat
+        
+        if proposedContentOffset.x <= 0 || collectionView.contentOffset == proposedContentOffset {
+            nextX = proposedContentOffset.x
+        } else {
+            nextX = collectionView.contentOffset.x + (velocity.x > 0 ? collectionView.bounds.size.width : -collectionView.bounds.size.width)
+        }
+        
+        let targetRect = CGRect(x: nextX, y: 0, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
+        
+        var offsetAdjustment = CGFloat.greatestFiniteMagnitude
+        
+        let horizontalOffset = proposedContentOffset.x + collectionView.contentInset.left
+        
+        let layoutAttributesArray = super.layoutAttributesForElements(in: targetRect)
+        
+        layoutAttributesArray?.forEach({ (layoutAttributes) in
+            let itemOffset = layoutAttributes.frame.origin.x
+            if fabsf(Float(itemOffset - horizontalOffset)) < fabsf(Float(offsetAdjustment)) {
+                offsetAdjustment = itemOffset - horizontalOffset
+            }
+        })
+        
+        return CGPoint(x: proposedContentOffset.x + offsetAdjustment, y: proposedContentOffset.y)
+    }
+}
+
+
